62a624fe71ef856d6345a245426ff72d03dfd0e2
Cat Cai <catherinec@fair.com>
2017-11-24 14:38:00 -0800

    WORK

--- grpc/validator.go
+++ grpc/validator.go
@@ -1,6 +1,9 @@
 package grpc
 
 import (
+	"encoding/json"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/dynamic"
@@ -19,7 +22,21 @@ func NewValidator() *Validator {
 
 func (v *Validator) Validate(messageDescriptor *desc.MessageDescriptor, request string) (*dynamic.Message, error) {
 	message := v.Factory.NewDynamicMessage(messageDescriptor)
-	err := (&runtime.JSONPb{}).Unmarshal([]byte(request), &message)
+	messageMap := map[string]interface{}{}
+	err := json.Unmarshal([]byte(request), &messageMap)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, err
+	}
+	for key, val := range messageMap {
+		spew.Dump(val)
+		field := messageDescriptor.FindFieldByName(key)
+		spew.Dump(field.AsFieldDescriptorProto())
+		intVal := val.(int)
+		message.SetFieldByName(key, intVal)
+	}
+	spew.Dump(message)
+	err = (&runtime.JSONPb{}).Unmarshal([]byte(request), &message)
 	if err != nil {
 		logger.Error(err.Error())
 		return nil, err
