bd7d9f186a5234eaed15b08e097682fe3cb485a1
Cat Cai <catherinec@fair.com>
2017-11-30 12:43:52 -0800

    Remove comments

--- grpc/constructor.go
+++ grpc/constructor.go
@@ -2,7 +2,6 @@ package grpc
 
 import (
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	//"github.com/catherinetcai/grpc-gateway/runtime"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/dynamic"
 )
@@ -20,12 +19,6 @@ func NewConstructor() *Constructor {
 
 func (v *Constructor) Construct(messageDescriptor *desc.MessageDescriptor, request string) (*dynamic.Message, error) {
 	message := v.Factory.NewDynamicMessage(messageDescriptor)
-	//	messageMap := map[string]interface{}{}
-	//	err := json.Unmarshal([]byte(request), &messageMap)
-	//	if err != nil {
-	//		logger.Error(err.Error())
-	//		return nil, err
-	//	}
 	err := (&runtime.JSONPb{}).Unmarshal([]byte(request), message)
 	if err != nil {
 		logger.Error(err.Error())
