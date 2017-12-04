b3d38f9698f6c6c2737c05de5139dc5bc202c9a7
Cat Cai <catherinec@fair.com>
2017-12-04 15:00:52 -0800

    Remove overriding known types.

--- grpc/constructor.go
+++ grpc/constructor.go
@@ -1,7 +1,6 @@
 package grpc
 
 import (
-	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/dynamic"
@@ -16,12 +15,10 @@ func NewConstructor() *Constructor {
 	constructor := &Constructor{
 		Factory: dynamic.NewMessageFactoryWithDefaults(),
 	}
-	constructor.overrideKnownTypes()
 	return constructor
 }
 
 func (c *Constructor) Construct(messageDescriptor *desc.MessageDescriptor, request string) (*dynamic.Message, error) {
-	// Find way to either edit this to use the timestamp.Timestamp{} type
 	message := c.Factory.NewDynamicMessage(messageDescriptor)
 	err := (&runtime.JSONPb{}).Unmarshal([]byte(request), message)
 	if err != nil {
@@ -29,18 +26,6 @@ func (c *Constructor) Construct(messageDescriptor *desc.MessageDescriptor, reque
 		return nil, err
 	}
 	return message, nil
-}
-
-// Override certain known types
-func (c *Constructor) overrideKnownTypes() {
-	registry := c.Factory.GetKnownTypeRegistry()
-	messageDescriptor, err := desc.LoadMessageDescriptorForMessage(&timestamp.Timestamp{})
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-	message := dynamic.NewMessage(messageDescriptor)
-	registry.AddKnownType(message)
 }
 
 func (c *Constructor) RegisterFileDescriptors(fileDescriptors []*desc.FileDescriptor) {
