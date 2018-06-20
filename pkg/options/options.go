e9721fde766b30b39db9f096b832ef18739c5f7e
Marshall Brekka <marshallsmedia@gmail.com>
2018-06-20 12:45:51 -0700

    Support some level of TLS. (#12)

---
+++ pkg/options/options.go
@@ -0,0 +1,46 @@
+package options
+
+import (
+	"crypto/tls"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+)
+
+type Options struct {
+	Metadata   metadata.MD
+	TLS        *TLS
+	Kubernetes *Kubernetes
+}
+
+func (o Options) DialOptions() []grpc.DialOption {
+	options := make([]grpc.DialOption, 0)
+
+	if o.TLS == nil {
+		options = append(options, grpc.WithInsecure())
+	} else {
+		options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(o.TLS.Config())))
+	}
+
+	return options
+}
+
+type TLS struct {
+	Insecure   bool
+	ServerName string
+}
+
+func (t TLS) Config() *tls.Config {
+	config := &tls.Config{}
+
+	config.InsecureSkipVerify = t.Insecure
+	config.ServerName = t.ServerName
+
+	return config
+}
+
+type Kubernetes struct {
+	Context   string
+	Namespace string
+}
